dfc72af6d04fad1a3ba1c06c8bd85e496b71870e
SophearithSaing <sophearithsaing123@gmail.com>
2026-06-14 21:44:06 +0700

    Add nav-bar component

---
+++ src/app/ui/navigation/nav-bar/nav-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'syn-nav-bar',
+  standalone: true,
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss',
+})
+export class SynNavBarComponent {}
