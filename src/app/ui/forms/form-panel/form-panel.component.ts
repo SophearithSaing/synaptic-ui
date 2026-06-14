ac01dd80dc8a99087ee46429b7cb1e1aa6430a91
SophearithSaing <sophearithsaing123@gmail.com>
2026-06-15 00:57:08 +0700

    Add additional support for form submit, error and footer

--- src/app/ui/forms/form-panel/form-panel.component.ts
+++ src/app/ui/forms/form-panel/form-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'syn-form-panel',
@@ -9,7 +9,20 @@ import { Component, Input } from '@angular/core';
 export class SynFormPanelComponent {
   @Input() public description = '';
 
+  @Input() public error: string | null = null;
+
   @Input({ required: true }) public title = '';
 
   @Input() public titleId: string | null = null;
+
+  @Output() public readonly formSubmit = new EventEmitter<SubmitEvent>();
+
+  /**
+   * Emits the native form submit event to the parent page.
+   *
+   * @param event Native form submit event.
+   */
+  public onSubmit(event: SubmitEvent): void {
+    this.formSubmit.emit(event);
+  }
 }
